c38c4d0886d118d5766bc12c44824c12901ec612
Versus Void <chaoskeeper@mail.ru>
2019-09-18 07:58:11 +0300

    fish prompt git branch

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -1,60 +1,61 @@
+if not set -q _set_color_normal
+	set -g _set_color_normal (set_color normal)
+end
+
+if not set -q _set_color_blue
+	set -g _set_color_blue (set_color --bold blue)
+end
+
+if not set -q _set_color_red
+	set -g _set_color_red (set_color --bold red)
+end
+
+if not set -q _set_color_brmagenta
+	set -g _set_color_brmagenta (set_color --dim brmagenta)
+end
+
+if not set -q _set_color_cwd
+	set -g _set_color_cwd (set_color $fish_color_cwd)
+end
+
 function fish_prompt --description 'Write out the prompt'
-	set stats $status $CMD_DURATION
-
-	if not set -q __fish_prompt_normal
-		set -g __fish_prompt_normal (set_color normal)
+	set -l stats $status $CMD_DURATION
+	if test $stats[1] != 0
+		printf "%s -> [%s]%s\n" $_set_color_red $stats[1] $_set_color_normal
 	end
 
-	if not set -q __fish_color_blue
-		set -g __fish_color_blue (set_color -o blue)
+	if test $stats[2] -gt 10000
+		set -l mil (math "$stats[2] % 1000")
+		set -l t (math -s0 "$stats[2] / 1000")
+
+		set -l s (math -s0 "$t % 60")
+		set -l t (math -s0 "$t / 60")
+
+		set -l m (math -s0 "$t % 60")
+		set -l h (math -s0 "$t / 60")
+		printf " in "
+		if test $h -gt 0
+			printf "%02d:%02d:" $h $m
+		else if test $m -gt 0
+			printf "%02d:" $m
+		end
+		printf "%02d.%s\n" $s $mil
 	end
 
-	switch $USER
-
-	case root toor
-
-		if not set -q __fish_prompt_cwd
-			if set -q fish_color_cwd_root
-				set -g __fish_prompt_cwd (set_color $fish_color_cwd_root)
-			else
-				set -g __fish_prompt_cwd (set_color $fish_color_cwd)
-			end
-		end
-
-		printf '%s@%s %s%s%s# ' $USER (prompt_hostname) "$__fish_prompt_cwd" (prompt_pwd) "$__fish_prompt_normal"
-
-	case '*'
-
-		if not set -q __fish_prompt_cwd
-			set -g __fish_prompt_cwd (set_color $fish_color_cwd)
-		end
-
-		if test $stats[1] != 0
-			printf "%s -> [%s]%s\n" (set_color -o red) $stats[1] (set_color normal)
-		end
-		if test $stats[2] -gt 10000
-			set -l mil (math "$stats[2] % 1000")
-			set -l t (math -s0 "$stats[2] / 1000")
-
-			set -l s (math -s0 "$t % 60")
-			set -l t (math -s0 "$t / 60")
-
-			set -l m (math -s0 "$t % 60")
-			set -l h (math -s0 "$t / 60")
-			printf " in "
-			if test $h -gt 0
-				printf "%02d:%02d:" $h $m
-			else if test $m -gt 0
-				printf "%02d:" $m
-			end
-			printf "%02d.%s\n" $s $mil
-		end
-		set -l ssh_note
-		if set -q SSH_TTY
-			set ssh_note "[ssh]"
-		end
-		set fish_emoji_width 2
-		printf '[%s] %s%s%s\f\r%s%s%s🐟 ' (date "+%H:%M:%S") "$__fish_prompt_cwd" (prompt_pwd) "$__fish_prompt_normal" \
-			(set_color -o red) $ssh_note (set_color normal)
+	set -l ssh_note
+	if set -q SSH_TTY
+		set ssh_note "[ssh]"
 	end
+
+	set -l branch
+	if test -e .git
+		set branch (git branch --show-current)' '
+	end
+
+	set fish_emoji_width 2
+	printf '[%s] %s%s%s%s%s%s\f\r%s%s%s🐟 ' \
+		(date "+%H:%M:%S") \
+		$_set_color_brmagenta $branch $_set_color_normal \
+		$_set_color_cwd (prompt_pwd) $_set_color_normal \
+		$_set_color_red $ssh_note $_set_color_normal
 end
